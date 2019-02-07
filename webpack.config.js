f9fa83b590395a0d06ee6fa10ffdb8a49be52315
Альберт <a.sahapov@innopolis.ru>
2019-02-07 18:30:45 +0300

    - configured webpack - set up webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './assets/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
+};
