1c3f98de3f5570a13a4ac0ea878e13baa76ffb40
Альберт <a.sahapov@innopolis.ru>
2019-02-19 23:45:46 +0300

    + Nav Bar + Grid list of Popular games + Design improvements

--- src/js/game-obj.js
+++ src/js/game-obj.js
@@ -1,35 +1,34 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import axios from 'axios'
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import Link from '@material-ui/core/Link';
+
 
 import style from '../styles.css'
 
 export const CharacterCard = ({ character }) => (
-    <Grid item sm={6} md={4} lg={3}>
-        <Card className={style.card}>
-            <CardMedia
-                className={style.media}
-                image={character.image}
-                title="Avatar"
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                    {character.name}
-                </Typography>
-                <Typography>
-                  //  Origin: {character.origin.name}
-                </Typography>
-            </CardContent>
-            <CardActions  className={style.bg}>
-                <Button size="small" color="primary">
-                    <span className={'class-name'}>Details</span>
-                </Button>
-            </CardActions>
-        </Card>
-    </Grid>
+      <GridListTile key={character.name} className={style.elem}>
+          <a href="#" className={style.coverlink}>
+            <div className={style.detailscover}>
+                Details
+            </div>
+
+            <img src={"https://images.igdb.com/igdb/image/upload/t_cover_big/" + character.cover.image_id + ".jpg"} alt={character.name} className={style.elem} />
+          </a>
+
+          <GridListTileBar
+            title={character.name}
+            subtitle={<span> {character.genres[0].name}</span>}
+            actionIcon={
+              <IconButton className={style.icon}>
+                Hey
+              </IconButton>
+            }
+          />
+      </GridListTile>
+
 )
--- src/js/mysecondcomp.js
+++ src/js/mysecondcomp.js
@@ -1,9 +1,17 @@
 import React, { Fragment } from "react"
 import axios from 'axios'
-import Button from '@material-ui/core/Button'
-import Grid from '@material-ui/core/Grid'
+import CircularProgress from '@material-ui/core/CircularProgress';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import Button from '@material-ui/core/Button';
 
 import { CharacterCard } from "./game-obj"
+import style from '../styles.css'
 
 export class MySecondComponent extends React.Component {
 
@@ -12,9 +20,8 @@ export class MySecondComponent extends React.Component {
     componentDidMount() {
         //axios.defaults.headers.common['Authorization'] = '861c079a35348acf2360c08a2efc2e90';
         axios
-            .get('https://cors-anywhere.herokuapp.com/http://api-v3.igdb.com/games/?fields=*,popularity&order=popularity:desc', {headers: {
-              "user-key": "861c079a35348acf2360c08a2efc2e90",
-              "Access-Control-Allow-Origin": "http://localhost:9000"
+            .get('https://cors-anywhere.herokuapp.com/http://api-v3.igdb.com/games/?fields=name,cover.*,genres.*,platforms, summary&limit=15&&expand=cover,genres&order=popularity:desc', {headers: {
+              "user-key": "861c079a35348acf2360c08a2efc2e90"
             }})
             .then(response => {
                  console.log('Axios returned', response.data)
@@ -22,19 +29,46 @@ export class MySecondComponent extends React.Component {
                   games: response.data
                 })
           });
+
       }
 
     render() {
         if(!this.state.games) {
-          return <h1>Loading</h1>
+          return <div className={style.loading}> <CircularProgress /> </div>
         }
 
         return (
-          <Grid container spacing={16}>
-              {this.state.games.map(game =>
-                  <CharacterCard key={game.name} character={game} />
-              )}
-          </Grid>
+          <div  className={style.cards} >
+          <AppBar position="static" color="default">
+            <Toolbar>
+              <Typography variant="h6" className={style.grow}>
+                GameCritic
+              </Typography>
+              <div className={style.search}>
+                <InputBase
+                  placeholder="Search…"
+                  classes={{
+                    root: style.inputRoot,
+                    input: style.inputInput,
+                  }}
+                />
+              </div>
+              <Button color="inherit">Home</Button>
+              <Button color="inherit">All Games</Button>
+              <Button color="inherit">Companies</Button>
+            </Toolbar>
+          </AppBar>
+
+          <GridList cellHeight={180} className={style.gridList} cellSpacing>
+            <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+              <ListSubheader component="div">Popular Now</ListSubheader>
+            </GridListTile>
+
+            {this.state.games.map(game =>
+              <CharacterCard key={game.name} character={game}/>
+            )}
+          </GridList>
+        </div>
         )
     }
 }
