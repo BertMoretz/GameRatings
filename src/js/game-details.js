f835c407b69121a22e5d4a376921c676837089fc
Альберт <a.sahapov@innopolis.ru>
2019-02-24 18:53:08 +0300

    + minor improvements

--- src/js/game-details.js
+++ src/js/game-details.js
@@ -11,7 +11,7 @@ import Grid from '@material-ui/core/Grid';
 import axios from 'axios';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-import styles from "../styles.css"
+import styles from "../details.css"
 
 export class GameDetails extends React.Component {
 
@@ -19,7 +19,7 @@ export class GameDetails extends React.Component {
 
     loadCharacterDetails = (gameID) => {
         axios
-            .get(`https://cors-anywhere.herokuapp.com/http://api-v3.igdb.com/games/${gameID}?fields=name,cover.*,artworks.*,age_ratings.*,created_at,game_engines.*,platforms.*,screenshots.*,genres.*,summary,aggregated_rating&&expand=cover,artworks,genres,screenshots,platforms,game_engines,age_ratings`, {headers: {
+            .get(`https://cors-anywhere.herokuapp.com/http://api-v3.igdb.com/games/${gameID}?fields=name,cover.*,artworks.*,age_ratings.*,created_at,game_engines.*,platforms.*,screenshots.*,genres.*,summary,aggregated_rating,involved_companies.company.*&&expand=cover,artworks,genres,screenshots,platforms,game_engines,age_ratings,involved_companies, involved_companies.company`, {headers: {
               "user-key": "861c079a35348acf2360c08a2efc2e90"
             }})
             .then(response => {
@@ -48,44 +48,17 @@ export class GameDetails extends React.Component {
         return (
             <Grid container justify="center" alignItems="center">
                 <Grid item xs={12} md={12} lg={12} justify="center">
-                    <Card className={styles.detailcard}>
-                        <CardMedia
-                            className={styles.detailedcover}
-                            image={"https://images.igdb.com/igdb/image/upload/t_screenshot_huge/" + game.screenshots[0].image_id  + ".jpg"}
-                            title="Cover"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                {game.name}
-                            </Typography>
-                            <Typography>
-                                Origin: {game.summary}
-                            </Typography>
-
-                            <hr/>
-
-                            <Table className={''}>
-                                <TableBody>
-                                    <TableRow>
-                                        <TableCell component="th" scope="row">genre</TableCell>
-                                        <TableCell align="right">{game.genres[0].name}</TableCell>
-                                    </TableRow>
-
-                                    <TableRow>
-                                        <TableCell component="th" scope="row">Release</TableCell>
-                                        <TableCell align="right">{game.created_at}</TableCell>
-                                    </TableRow>
-
-                                    <TableRow>
-                                        <TableCell component="th" scope="row">Rating</TableCell>
-                                        <TableCell align="right">{game.aggregated_rating}</TableCell>
-                                    </TableRow>
-                                </TableBody>
-                            </Table>
-
-                        </CardContent>
-                    </Card>
-                </Grid>
+                  <div className={styles.hey}>
+                    <img src={"https://images.igdb.com/igdb/image/upload/t_screenshot_huge/" + game.screenshots[0].image_id  + ".jpg"} className={styles.back}/>
+                  </div>
+                  <div className={styles.gamecover}>
+                    <img src={"https://images.igdb.com/igdb/image/upload/t_cover_big/" + game.cover.image_id + ".jpg"} alt={game.name} className={styles.elem} />
+                    <div className={styles.gamename}> {game.name} </div>
+                    <div className={styles.gamegenre}> {game.genres[0].name} </div>
+                    <div className={styles.gamecomp}> Developed by {game.involved_companies[0].company.name} </div>
+                    <div className={styles.agerating}> <img src={game.age_ratings.rating_cover_url} /></div>
+                  </div>
+              </Grid>
             </Grid>
         )
     }
