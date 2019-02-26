7a802f9d44e88c147b95b0d2bc993127d356fc20
Альберт <a.sahapov@innopolis.ru>
2019-02-26 19:21:30 +0300

    + minor design improvements + added screenshots in game details

--- src/js/game-details.js
+++ src/js/game-details.js
@@ -8,6 +8,10 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 import Grid from '@material-ui/core/Grid';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import Paper from '@material-ui/core/Paper';
 import axios from 'axios';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
@@ -19,7 +23,7 @@ export class GameDetails extends React.Component {
 
     loadCharacterDetails = (gameID) => {
         axios
-            .get(`https://cors-anywhere.herokuapp.com/http://api-v3.igdb.com/games/${gameID}?fields=name,cover.*,artworks.*,age_ratings.*,created_at,game_engines.*,platforms.*,screenshots.*,genres.*,summary,aggregated_rating,involved_companies.company.*&&expand=cover,artworks,genres,screenshots,platforms,game_engines,age_ratings,involved_companies, involved_companies.company`, {headers: {
+            .get(`https://cors-anywhere.herokuapp.com/http://api-v3.igdb.com/games/${gameID}?fields=name,cover.*,artworks.*,created_at,game_engines.*,platforms.*,screenshots.*,genres.*,summary,aggregated_rating,involved_companies.company.*&&expand=cover,artworks,genres,screenshots,platforms,game_engines,involved_companies, involved_companies.company`, {headers: {
               "user-key": "861c079a35348acf2360c08a2efc2e90"
             }})
             .then(response => {
@@ -52,12 +56,39 @@ export class GameDetails extends React.Component {
                     <img src={"https://images.igdb.com/igdb/image/upload/t_screenshot_huge/" + game.screenshots[0].image_id  + ".jpg"} className={styles.back}/>
                   </div>
                   <div className={styles.gamecover}>
-                    <img src={"https://images.igdb.com/igdb/image/upload/t_cover_big/" + game.cover.image_id + ".jpg"} alt={game.name} className={styles.elem} />
-                    <div className={styles.gamename}> {game.name} </div>
-                    <div className={styles.gamegenre}> {game.genres[0].name} </div>
-                    <div className={styles.gamecomp}> Developed by {game.involved_companies[0].company.name} </div>
-                    <div className={styles.agerating}> <img src={game.age_ratings.rating_cover_url} /></div>
+                    <Grid container justify="center" alignItems="center">
+                      <Grid item xs={12} md={4} lg={3} justify="center">
+                        <img src={"https://images.igdb.com/igdb/image/upload/t_cover_big/" + game.cover.image_id + ".jpg"} alt={game.name} className={styles.elem} />
+                      </Grid>
+                      <Grid item xs={12} sm container>
+                        <Grid item xs={12} sm container direction="column" spacing={40}>
+                          <Grid item >
+                            <div className={styles.gamename}> {game.name} </div>
+                            <div className={styles.gamegenre}> {game.genres[0].name} </div>
+                            <br/>
+                            <div className={styles.gamecomp}> Developed by {game.involved_companies[0].company.name} </div>
+                          </Grid>
+                          <Grid item className={styles.hm}>
+                            <div className={styles.rate}> {game.summary} </div>
+                          </Grid>
+
+                        </Grid>
+                      </Grid>
+
+                      <Grid item xs={12} md={4} lg={3} alignItems="flex-end">
+                        <div className={styles.gamerating}> { ('aggregated_rating' in game)? parseInt(game.aggregated_rating )+"%" : "tbd" } </div>
+                      </Grid>
+                    </Grid>
                   </div>
+                  <div className="styles.screens">
+                    <GridList className={styles.gridList} cols={2.3} cellHeight={300}>
+                      {game.screenshots.map(screen => (
+                        <GridListTile key={screen.image_id}>
+                          <img src={"https://images.igdb.com/igdb/image/upload/t_screenshot_med/" + screen.image_id + ".jpg"} alt={screen.image_id}/>
+                        </GridListTile>
+                      ))}
+                    </GridList>
+                </div>
               </Grid>
             </Grid>
         )
