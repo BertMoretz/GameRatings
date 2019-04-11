d324547a5afa8b44b846db1bfa58809e80910549
Альберт <a.sahapov@innopolis.ru>
2019-04-11 18:52:12 +0300

    + some tests added

---
+++ src/pages/details/tests/game-details.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import createStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import GameDetails from '../game-details'
+
+describe('GameDetails', () => {
+	   it('test render empty component', () => {
+	       const initialState = {
+	           game: void 0
+	       }
+
+	       const container = renderer
+	           .create(
+	                   <Router>
+	                       <Route component={GameDetails}/>
+	                   </Router>
+	           )
+
+	       expect(container.toJSON()).toMatchSnapshot()
+	   })
+
+})
