30226ac2f7c13e04dc3a7bef80207e92dcf7d205
albert <arsakha3@mts.ru>
2019-11-18 18:11:11 +0300

    + minor improvements and fixes

--- src/pages/allgames/all-games.js
+++ src/pages/allgames/all-games.js
@@ -25,7 +25,7 @@ export class AllGames extends React.Component {
     loadGames = (offset) => {
       axios
           .get(`https://cors-anywhere.herokuapp.com/http://api-v3.igdb.com/games/?fields=name,cover.*,genres.*,platforms,popularity,summary,aggregated_rating&limit=15&offset=${offset}&&expand=cover,genres&order=popularity:desc`, {headers: {
-            "user-key": "861c079a35348acf2360c08a2efc2e90"
+            "user-key": "bf64f9cdc5dcd51330d432e658773047"
           }})
           .then(response => {
             console.log('Axios returned', response.data)
